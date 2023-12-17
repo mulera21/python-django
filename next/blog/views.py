226e7075f7699ccc1c6318a45387c07b720bde39
ikumwa edmond mulera <edmondmulera55@gmail.com>
2023-12-17 10:11:55 +0300

    django

--- next/blog/views.py
+++ next/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1>Blog home</h1>")
+
+def nice(request):
+    return HttpResponse("<h1>service</h1>")
+
+def fly(request):
+    return HttpResponse("<h1> contact </h1>")
